9dc4c941234cffa7d8c5e8d5e778dcd8e1c2d5dd
jhwk109as <jhwk109@korea.ac.kr>
2018-11-16 00:26:01 +0900

    update

--- src/Project.java
+++ src/Project.java
@@ -1,9 +1,20 @@
+import java.util.*;
+
+class Account{
+	String name;
+	int money;
+	
+}
+
+
+
 
 public class Project {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
