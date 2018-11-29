413f75b0e8d27564bb6cdff8358419cdf418e85f
jhwk109as <jhwk109@korea.ac.kr>
2018-11-29 23:58:16 +0900

    update

--- src/Project.java
+++ src/Project.java
@@ -15,48 +15,51 @@ public class Project {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		String account = sc.next(); 
-				
+		System.out.println("이름을 입력하세요>>");
+		String name = sc.next();
+		Account account = new Account();
+		account.name = name;
 		System.out.print("수입(1), 지출(2), 내역(3), 종료(4)\n>>");
 		
 		switch(sc.nextInt()) {
 		case 1: 
 			System.out.println("금액 입력>>");
 			int m = sc.nextInt();
-			//account.money += m 잔액증가
+			account.money += m;
 			System.out.println("내용 입력>>");
 			String usage = sc.next();
-			all.add(m + "원, " + usage);
-			income.add(m + "원, " + usage);
+			account.all.add(m + "원, " + usage);
+			account.income.add(m + "원, " + usage);
 			
 			break;
 		case 2: 
 			System.out.println("금액 입력>>");
-			int m = sc.nextInt();
-			//account.money -= m 잔액감소 
+			m = sc.nextInt();
+			account.money -= m; 
 			System.out.println("내용 입력>>");
-			String usage = sc.next();
-			all.add(m + "원, " + usage);
-			consume.add(m + "원, " + usage);
+			usage = sc.next();
+			account.all.add(m + "원, " + usage);
+			account.consume.add(m + "원, " + usage);
 			
 			break;
 		case 3: 
 			System.out.println("수입 내역(1), 지출 내역(2), 전체 내역(3)");
 			switch(sc.nextInt()) {
 			case 1: 
-				Iterator<String> incomeit = income.Iterator();
+				Iterator<String> incomeit = account.income.iterator();
+				
 				while(incomeit.hasNext())
-					System.out.println(income.next());
+					System.out.println(incomeit.next());
 				break;
 			case 2: 
-				Iterator<String> consumeit = consume.Iterator();
+				Iterator<String> consumeit = account.consume.iterator();
 				while(consumeit.hasNext())
-					System.out.println(consume.next());
+					System.out.println(consumeit.next());
 				break;
 			case 3: 
-				Iterator<String> allit = all.Iterator();
+				Iterator<String> allit = account.all.iterator();
 				while(allit.hasNext())
-					System.out.println(all.next());
+					System.out.println(allit.next());
 				break;
 			}
 			break;
