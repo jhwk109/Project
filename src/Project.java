f822646b187e484c9957df67b2b2ea0653d353cb
jhwk109as <jhwk109@korea.ac.kr>
2018-12-13 00:37:59 +0900

    update

--- src/Project.java
+++ src/Project.java
@@ -1,81 +1,126 @@
-
 import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
-class Account{
-	String name;
-	int money;
-	ArrayList<String> all = new ArrayList<String>();
-	ArrayList<String> income = new ArrayList<String>();
-	ArrayList<String> consume = new ArrayList<String>();
-}
-
-
-public class Project {
-
+public class Project extends JFrame {
+	
+	public Project() {
+		setTitle("가계부");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(new MyPanel());
+		
+		setSize(600, 250);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		Account account = new Account();
-		System.out.println("이름을 입력하세요>>");
-		account.name = sc.next();		
-		System.out.println("잔액을 입력하세요>>");
-		account.money = sc.nextInt();		
-		
-		while(true) {
-				System.out.print("수입(1), 지출(2), 내역(3), 종료(4)\n>>");
-				switch(sc.nextInt()) {
-					case 1: 
-							System.out.println("금액 입력>>");
-							int m = sc.nextInt();
-							account.money += m;
-							System.out.println("내용 입력>>");
-							String income = sc.next();
-							account.all.add("+" + m + "원, " + income);
-							account.income.add("+" + m + "원, " + income);
-			
-							break;
-					case 2: 
-							System.out.println("금액 입력>>");
-							m = sc.nextInt();
-							account.money -= m; 
-							System.out.println("내용 입력>>");
-							String usage = sc.next();
-							account.all.add("-" + m + "원, " + usage);
-							account.consume.add("-" + m + "원, " + usage);
-			
-							break;
-					case 3: 
-							System.out.println("수입 내역(1), 지출 내역(2), 전체 내역(3)\n>>");
-							switch(sc.nextInt()) {
-								case 1: 
-										Iterator<String> incomeit = account.income.iterator();				
-										while(incomeit.hasNext())
-											System.out.println(incomeit.next());
-										System.out.println("잔액: " + account.money);
-										System.out.print("\n");
-										break;
-								case 2: 
-										Iterator<String> consumeit = account.consume.iterator();
-										while(consumeit.hasNext())
-											System.out.println(consumeit.next());
-										System.out.println("잔액: " + account.money);
-										System.out.print("\n");
-										break;
-								case 3: 
-										Iterator<String> allit = account.all.iterator();
-										while(allit.hasNext())
-											System.out.println(allit.next());
-										System.out.println("잔액: " + account.money);
-										System.out.print("\n");
-										break;
-							}
-							break;
-					case 4: System.out.println("프로그램 종료"); 
-							sc.close();
-							System.exit(0);
-				}		
-		
-		}
+		new Project(); 		
 	}
-
 }
+
+class MyPanel extends Panel {	
+	
+	int money;
+	ArrayList<String> income = new ArrayList<String>();
+	ArrayList<String> expense = new ArrayList<String>();
+	
+	private JButton ib = new JButton("수입");
+	private JButton eb = new JButton("지출");
+	private JButton wb = new JButton("내역 저장");
+	private JTextArea ta = new JTextArea(7, 30);
+	private JTextField tf = new JTextField(20);
+	
+	int amount;
+	String usage;
+	
+	public MyPanel() {
+		
+		String dirStr = "c:\\temp\\가계부";
+		File newDir = new File(dirStr);
+		if(newDir.exists()!= true) newDir.mkdir();		
+		
+		String date = JOptionPane.showInputDialog("날짜를 입력하세요.(X월X일)");
+		
+		File file = new File("c:\\temp\\가계부\\" + date + ".txt");	
+		money = Integer.parseInt(JOptionPane.showInputDialog("잔액을 입력하세요."));			
+				
+		this.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 20));
+		add(ib);
+		add(eb);
+		add(wb);
+		add(new JScrollPane(ta));
+		add(new JLabel("잔액 : "));
+		add(tf);
+		tf.setText(Integer.toString(money));
+		
+		ib.addActionListener(new ActionListener() {
+					
+			public void actionPerformed(ActionEvent e) {
+				String howmuch = JOptionPane.showInputDialog("금액을 입력하세요.");
+				if(howmuch != null)
+					amount = Integer.parseInt(howmuch);
+				String what = JOptionPane.showInputDialog("내용을 입력하세요.");
+				if(what != null)
+					usage = what;				
+				money += amount;
+				income.add(amount + ", " + what);
+				ta.append("+" + amount + ", " + what + "\n");
+				tf.setText(Integer.toString(money));
+				
+			}
+		});
+		
+		eb.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				String howmuch = JOptionPane.showInputDialog("금액을 입력하세요.");
+				if(howmuch != null)
+					amount = Integer.parseInt(howmuch);
+				String what = JOptionPane.showInputDialog("내용을 입력하세요.");
+				if(what != null)
+					usage = what;
+				money -= amount;
+				expense.add(amount + ", " + usage);
+				ta.append("-" + amount + ", " + usage + "\n");
+				tf.setText(Integer.toString(money));
+			}
+		});		
+		
+		wb.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				Iterator<String> incomeit = income.iterator();
+				Iterator<String> expenseit = expense.iterator();
+				FileWriter fout = null;
+				try {
+					fout = new FileWriter(file);
+					fout.write("수입\r\n");
+					while(incomeit.hasNext()) {
+						fout.write(incomeit.next());
+						fout.write("\r\n");
+					}
+					fout.write("\r\n");
+					fout.write("지출\r\n");
+					while(expenseit.hasNext()) {
+						fout.write(expenseit.next());
+						fout.write("\r\n");
+					}					
+					income.clear();
+					expense.clear();
+					
+					fout.write("\r\n");
+					fout.write("잔액 : " + money);
+					fout.close();
+					System.exit(0);
+				} catch (IOException e1) {
+					System.out.println("입출력 오류");
+				}
+			}
+		});
+		
+	}
+}
+
