e7e206c439d6065bc2151d33dc69d3a78c94c308
jhwk109as <jhwk109@korea.ac.kr>
2018-12-13 23:27:02 +0900

    update

--- src/Project.java
+++ src/Project.java
@@ -31,7 +31,7 @@ class MyPanel extends Panel {
 	private JButton eb = new JButton("지출");
 	private JButton wb = new JButton("내역 저장");
 	private JTextArea ta = new JTextArea(7, 30);
-	private JTextField tf = new JTextField(20);
+	private JLabel tf = new JLabel();
 	
 	int amount;
 	String usage;
@@ -46,7 +46,7 @@ class MyPanel extends Panel {
 		
 		File file = new File("c:\\temp\\가계부\\" + date + ".txt");	
 		money = Integer.parseInt(JOptionPane.showInputDialog("잔액을 입력하세요."));			
-				
+		tf.setText(Integer.toString(money));
 		this.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 20));
 		add(ib);
 		add(eb);
@@ -54,22 +54,21 @@ class MyPanel extends Panel {
 		add(new JScrollPane(ta));
 		add(new JLabel("잔액 : "));
 		add(tf);
-		tf.setText(Integer.toString(money));
 		
 		ib.addActionListener(new ActionListener() {
 					
 			public void actionPerformed(ActionEvent e) {
 				String howmuch = JOptionPane.showInputDialog("금액을 입력하세요.");
-				if(howmuch != null)
+				if(howmuch != null) {
 					amount = Integer.parseInt(howmuch);
-				String what = JOptionPane.showInputDialog("내용을 입력하세요.");
-				if(what != null)
-					usage = what;				
-				money += amount;
-				income.add(amount + ", " + what);
-				ta.append("+" + amount + ", " + what + "\n");
-				tf.setText(Integer.toString(money));
-				
+					String what = JOptionPane.showInputDialog("내용을 입력하세요.");
+					if(what != null)
+						usage = what;				
+					money += amount;
+					income.add(amount + ", " + what);
+					ta.append("+" + amount + ", " + what + "\n");
+					tf.setText(Integer.toString(money));
+				}
 			}
 		});
 		
@@ -77,15 +76,17 @@ class MyPanel extends Panel {
 			
 			public void actionPerformed(ActionEvent e) {
 				String howmuch = JOptionPane.showInputDialog("금액을 입력하세요.");
-				if(howmuch != null)
+				if(howmuch != null) {
 					amount = Integer.parseInt(howmuch);
-				String what = JOptionPane.showInputDialog("내용을 입력하세요.");
-				if(what != null)
-					usage = what;
-				money -= amount;
-				expense.add(amount + ", " + usage);
-				ta.append("-" + amount + ", " + usage + "\n");
-				tf.setText(Integer.toString(money));
+					String what = JOptionPane.showInputDialog("내용을 입력하세요.");
+					if(what != null)
+						usage = what;
+					money -= amount;
+					expense.add(amount + ", " + usage);
+					ta.append("-" + amount + ", " + usage + "\n");
+					tf.setText(Integer.toString(money));
+				}
+				
 			}
 		});		
 		
@@ -99,12 +100,14 @@ class MyPanel extends Panel {
 					fout = new FileWriter(file);
 					fout.write("수입\r\n");
 					while(incomeit.hasNext()) {
+						fout.write("+");
 						fout.write(incomeit.next());
 						fout.write("\r\n");
 					}
 					fout.write("\r\n");
 					fout.write("지출\r\n");
 					while(expenseit.hasNext()) {
+						fout.write("-");
 						fout.write(expenseit.next());
 						fout.write("\r\n");
 					}					
